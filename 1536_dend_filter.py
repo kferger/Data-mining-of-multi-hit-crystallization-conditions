9ac297e366a43474ce8e2bde4fe22268b7d2d761
kferger <39772509+kferger@users.noreply.github.com>
2019-08-01 14:01:56 -0400

    Update 1536_dend_filter.py

--- 1536_dend_filter.py
+++ 1536_dend_filter.py
@@ -5,6 +5,10 @@ import pandas as pd
 from itertools import combinations_with_replacement, repeat
 import numpy as np
 
+"""
+Extract all cocktails from 1536 screen for sample 1055
+"""
+
 os.environ['XTUITION_TOKEN'] = '9d89e79c-ed4d-11e5-8042-00270e10b7a7'
 ROOT_PATH = "/projects/academic/esnell/kferger"
 
